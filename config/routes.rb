f1ea28bbc305bcde7722e2a6bf178debc0595370
Mateusz Piechowiak <mateusz.s.piechowiak@gmail.com>
2015-10-16 12:37:39 +0200

    Add missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root 'visitors#index'
+
   resources :students do
+    get :subjects
+  end
+
+  resources :teachers do
     get :subjects
   end
 
